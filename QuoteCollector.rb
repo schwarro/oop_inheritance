4f20eaea14d012d7e191f7c9062160d7d4db92cd
schwarro <rob.ap.schwartz@gmail.com>
2018-07-27 12:33:47 -0400

    completed quote collector exercise

--- QuoteCollector.rb
+++ QuoteCollector.rb
@@ -2,4 +2,27 @@ require_relative "multilinguist"
 
 class QuoteCollector < Multilinguist
 
+  def initialize
+    @quotes = []
+  end
+
+  def add_quote(quote)
+    @quotes << quote
+  end
+
+  def all
+    return @quotes
+  end
+
+  def random
+    say_quote = @quotes.sample
+    self.say_in_local_language(say_quote)
+  end
 end
+
+collect1 = QuoteCollector.new
+collect1.add_quote('Hello my name is Rob')
+collect1.add_quote('Which way to the market?')
+collect1.add_quote('Where do you want to eat today?')
+collect1.travel_to("Italy")
+p collect1.random
